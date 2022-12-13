2f1908bfd1f478418141e5db1f22b53eb2df27da
XadicaAllahverdi <XadicaAllahverdi@bitbucket.org>
2022-12-13 20:10:41 +0400

    delete components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { MyHeaderComponent } from './my-header/my-header.component';
-import { MyMiddleComponent } from './my-middle/my-middle.component';
-import { MyFooterComponent } from './my-footer/my-footer.component';
-import { HomeComponent } from './home/home.component';
-import { SecondComponent } from './second/second.component';
-import { ThirdComponent } from './third/third.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MyHeaderComponent,
-    MyMiddleComponent,
-    MyFooterComponent,
-    HomeComponent,
-    SecondComponent,
-    ThirdComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
