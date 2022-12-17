65a14e9dc61271093744678c95aa93366483946a
Murad_Karimov <muradhafizoglu.kr@gmail.com>
2022-12-17 11:18:31 +0400

    services page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ServicesComponent } from './services/services.component';
 
 const routes: Routes = [
+  { path: 'services', component: ServicesComponent }
 ];
 
 @NgModule({
