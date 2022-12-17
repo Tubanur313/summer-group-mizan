7b2e9b1c80fa918d6a508b53c9295123f1ea0d01
XadicaAllahverdi <XadicaAllahverdi@bitbucket.org>
2022-12-17 12:46:52 +0400

    corrections

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { MyHomeComponent } from './my-home/my-home.component';
 import { ServicesComponent } from './services/services.component';
+import { SiteElementsComponent } from './site-elements/site-elements.component';
 
 const routes: Routes = [
   { path: '', component: MyHomeComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'contact-us', component: ContactUsComponent },
-  { path: 'about-us', component: AboutUsComponent }
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'elements', component: SiteElementsComponent }
 ];
 
 @NgModule({
