c2023ba6f67d9855a47a86bd1492b8424be09e91
FarasatN <ferasetnovruzov1@gmail.com>
2023-01-17 00:15:18 +0400

    finally, I have finished :)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AccordComponent } from './accord/accord.component';
 import { ElementsComponent } from './elements/elements.component';
-// import { DialogComponent } from './dialog/dialog.component';
-// import { ElementsComponent } from './elements/elements.component';
-// import { FaqComponent } from './faq/faq.component';
-// import { HomeComponent } from './home/home.component';
-// import { MyBsMainComponent } from './my-bs-main/my-bs-main.component';
-// import { MyTabMainComponent } from './my-tab-main/my-tab-main.component';
-// import { SiteElementsComponent } from './site-elements/site-elements.component';
+
 
 const routes: Routes = [
   // { path: '',   redirectTo: 'elements', pathMatch: 'full' },
   { path: 'elements', component: ElementsComponent },
-  { path: 'accord', component: AccordComponent },
 
-
-  // { path: 'faq', component: FaqComponent },
-  // { path: 'dialog', component: DialogComponent },
-  // { path: 'bottom-sheet', component: MyBsMainComponent },
-  // { path: 'tab', component: MyTabMainComponent },
-  // { path: 'elements', component: ElementsComponent },
-  // { path: 'site-elements', component: SiteElementsComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,41 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-// import { SiteElementsComponent } from './site-elements/site-elements.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { HomeComponent } from './home/home.component';
-// import { FaqComponent } from './faq/faq.component';
-// import { DialogComponent } from './dialog/dialog.component';
-// import { DiceComponent } from './entry-components/dice/dice.component';
-// import { ElementsComponent } from './elements/elements.component';
 import { MatDialogModule } from '@angular/material/dialog';
-// import { MyBsMainComponent } from './my-bs-main/my-bs-main.component';
-// import { MyBsSubComponent } from './entry-components/my-bs-sub/my-bs-sub.component';
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
-// import { MyTabMainComponent } from './my-tab-main/my-tab-main.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatBadgeModule } from '@angular/material/badge';
 import { ElementsComponent } from './elements/elements.component';
-import { AccordComponent } from './accord/accord.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ElementsComponent,
-    AccordComponent,
-    // SiteElementsComponent,
-    // HomeComponent,
-    // FaqComponent,
-    // DialogComponent,
-    // DiceComponent,
-    // ElementsComponent,
-    // MyBsMainComponent,
-    // MyBsSubComponent,
-    // MyTabMainComponent,
   ],
-  // entryComponents: [DiceComponent,
-    // MyBsSubComponent,
-  // ],
 
   imports: [
     BrowserModule,
